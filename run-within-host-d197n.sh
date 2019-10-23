efc45e70b94c8e0f43746b168399beac05a4bfbe
Alex Zarebski <aezarebski@gmail.com>
2019-10-23 16:42:46 +0100

    Check that the output directory exists before running any analysis

--- run-within-host-d197n.sh
+++ run-within-host-d197n.sh
@@ -9,6 +9,13 @@ OUTPUT_FILE_R3=out/within-host/fit-d197n-relax-3.rds
 OUTPUT_FILE_R2=out/within-host/fit-d197n-relax-2.rds
 OUTPUT_FILE_R1=out/within-host/fit-d197n-relax-1.rds
 OUTPUT_DIR=out/within-host
+
+# If the output directory is missing then exit early with a message.
+if [ ! -d $OUTPUT_DIR ]; then
+    echo "ERROR: Could not find output directory: $OUTPUT_DIR"
+    exit 1
+fi
+
 echo "Running the demonstration code for the within-host model"
 echo "Pre-processing data"
 Rscript main.R --within_dump --xlsx $XLSX_FILE --dump $DUMP_FILE_R3 --relax 3
